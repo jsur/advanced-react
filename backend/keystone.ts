465b5bd86f5104677808ff473f22dfa5a553dc6e
julius.suominen <julius.suominen@gmail.com>
2022-01-08 20:20:55 +0200

    video 15 done

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,13 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { KeystoneContext } from '@keystone-next/types';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -9,22 +17,39 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseUrl,
-    // TODO: add data seeding
-  },
-  lists: createSchema({
-    // schema items
-  }),
-  ui: {
-    isAccessAllowed: () => true,
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // add initial roles
   },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      // TODO: add data seeding
+    },
+    lists: createSchema({
+      User,
+      Product,
+    }),
+    ui: {
+      isAccessAllowed: (ctx: KeystoneContext) => !!ctx.session.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL query
+      User: 'id',
+    }),
+  })
+);
