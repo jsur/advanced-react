90ac11f005a5b4fea9b3ef85191ce9e896bab2b3
julius.suominen <julius.suominen@gmail.com>
2022-02-15 15:47:06 +0200

    video 27 done

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,8 +1,10 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import Router from 'next/router';
 import DisplayError from './ErrorMessage';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -40,17 +42,21 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [
+        {
+          query: ALL_PRODUCTS_QUERY,
+        },
+      ],
     }
   );
-
-  console.log(createProduct);
 
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
-        await createProduct();
+        const { data: cpData } = await createProduct();
         clearForm();
+        Router.push({ pathname: `/products/${cpData.createProduct.id}` });
       }}
     >
       <DisplayError error={error} />
